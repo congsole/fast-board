5416b2c9c4a2b5650dc41c293a9ce8edb0ce4417
congsole <qax741@gmail.com>
2024-01-15 01:11:24 +0900

    #61 - 리팩토링: 엔티티 연관관계 조인컬럼 설정

--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -26,11 +26,12 @@ public class ArticleComment extends AuditingFields {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(optional = false)
     @Setter
+    @ManyToOne(optional = false)
     private Article article; // 게시글 (ID)
-    @ManyToOne(optional = false)
     @Setter
+    @ManyToOne(optional = false)
+    @JoinColumn(name="userId")
     private UserAccount userAccount;
 
     @Column(nullable = false, length = 500)
