5cf3b9b008ba14ef52dbe0c5a1750359eb02315f
congsole <qax741@gmail.com>
2024-01-15 01:19:14 +0900

    #61 - 리팩토링: equals, hashcode에서 id필드에 직접 접근하는 대신 getter로 변경

--- src/main/java/com/fastcampus/projectboard/domain/UserAccount.java
+++ src/main/java/com/fastcampus/projectboard/domain/UserAccount.java
@@ -1,14 +1,11 @@
 package com.fastcampus.projectboard.domain;
-
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
 import java.util.Objects;
-
 @Getter
-@ToString(callSuper = true)
+@ToString
 @Table(indexes = {
         @Index(columnList = "email", unique = true),
         @Index(columnList = "createdAt"),
@@ -19,34 +16,21 @@ public class UserAccount extends AuditingFields {
     @Id
     @Column(length = 50)
     private String userId;
-
     @Setter @Column(nullable = false) private String userPassword;
-
     @Setter @Column(length = 100) private String email;
     @Setter @Column(length = 100) private String nickname;
     @Setter private String memo;
-
-
     protected UserAccount() {}
-
-    private UserAccount(String userId, String userPassword, String email, String nickname, String memo, String createdBy) {
+    private UserAccount(String userId, String userPassword, String email, String nickname, String memo) {
         this.userId = userId;
         this.userPassword = userPassword;
         this.email = email;
         this.nickname = nickname;
         this.memo = memo;
-        this.createdBy = createdBy;
-        this.modifiedBy = createdBy;
     }
-
     public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo) {
-        return UserAccount.of(userId, userPassword, email, nickname, memo, null);
+        return new UserAccount(userId, userPassword, email, nickname, memo);
     }
-
-    public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo, String createdBy) {
-        return new UserAccount(userId, userPassword, email, nickname, memo, createdBy);
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
