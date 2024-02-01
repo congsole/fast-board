5521c5098367dc82a97490346b1e9d8c2ba80150
congsole <qax741@gmail.com>
2024-02-01 22:58:16 +0900

    naver platform cloud의 GeoLocation 호출 결과를 dto로 변환 성공
    뷰를 통해서 확인했다.

--- src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
+++ src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package com.fastcampus.projectboard.controller;
 
 import com.fastcampus.projectboard.domain.constant.FormStatus;
+import com.fastcampus.projectboard.dto.GeoLocationResponseDto;
 import com.fastcampus.projectboard.dto.UserAccountDto;
 import com.fastcampus.projectboard.dto.request.ArticleRequest;
 import com.fastcampus.projectboard.dto.response.ArticleResponse;
@@ -8,20 +9,31 @@ import com.fastcampus.projectboard.dto.response.ArticleWithCommentsResponse;
 import com.fastcampus.projectboard.domain.constant.SearchType;
 import com.fastcampus.projectboard.security.BoardPrincipal;
 import com.fastcampus.projectboard.service.ArticleService;
+import com.fastcampus.projectboard.service.GeoLocationService;
 import com.fastcampus.projectboard.service.PaginationService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/articles")
@@ -29,6 +41,7 @@ public class ArticleController {
 
     private final ArticleService articleService;
     private final PaginationService paginationService;
+    private final GeoLocationService geoLocationService;
 
     @GetMapping
     public String articles(
@@ -76,8 +89,53 @@ public class ArticleController {
     }
 
     @GetMapping("/form")
-    public String articleForm(ModelMap map) {
+    public String articleForm(ModelMap map, HttpServletRequest request) throws Exception {
         map.addAttribute("formStatus", FormStatus.CREATE);
+        // 사용자 ip 받아오기.
+        String ip = request.getHeader("X-Forwarded-For");
+        log.info("X-FORWARDED-FOR : " + ip);
+
+        if (ip == null) {
+            ip = request.getHeader("Proxy-Client-IP");
+            log.info("Proxy-Client-IP : " + ip);
+        }
+        if (ip == null) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+            log.info("WL-Proxy-Client-IP : " + ip);
+        }
+        if (ip == null) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+            log.info("HTTP_CLIENT_IP : " + ip);
+        }
+        if (ip == null) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+            log.info("HTTP_X_FORWARDED_FOR : " + ip);
+        }
+        if (ip == null) {
+            ip = request.getRemoteAddr();
+            log.info("getRemoteAddr : "+ip);
+        }
+        log.info("Result : IP Address : "+ip);
+
+        /****************/
+        ip = "221.147.174.40";
+        //TODO 삭제필요함, 테스트용이었음.
+        /****************/
+
+        String json = geoLocationService.getNaverGeoLocation(ip);
+        GeoLocationResponseDto geoLocationResponseDto;
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            GeoLocationResponseDto dto = objectMapper.readValue(json, GeoLocationResponseDto.class);
+            geoLocationResponseDto = dto;
+
+        } catch(Exception e) {
+            throw new Exception(e.getMessage());
+        }
+
+        map.addAttribute("geoLocationResponseDto", geoLocationResponseDto);
+
         return "articles/form";
     }
 
